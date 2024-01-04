eaaff3d271f240c84b57294d5d710fcf8fb420a3
Huston Bryant <Hustonbryant22@gmail.com>
2024-01-04 01:44:20 -0500

    https://github.com/FlareC0de/Earthy_Responsive_website

---
+++ script.js
@@ -0,0 +1,22 @@
+const hamburger = document.querySelector("#icon");
+const nav = document.querySelector(".nav-mobile");
+
+hamburger.addEventListener("click", function() {
+    hamburger.classList.toggle("bx-x");
+    nav.classList.toggle("active");
+});
+
+//hide mobile navigation after clicking a link or clicking outside
+document.onclick = function(n){
+    if(n.target.id !== "icon" && n.target.id !== "nav-mobile"){
+        hamburger.classList.remove("bx-x");
+        nav.classList.remove("active");
+    }
+}
+
+//adding sticky animation to the navigation
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky", scrollY > 0);
+})
